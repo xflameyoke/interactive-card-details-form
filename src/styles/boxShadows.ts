60f07b899b0f6fb50caa16400bc3a17f2dfea18e
Mateusz Łuczak <luczakmmateusz@gmail.com>
2023-09-18 23:01:34 +0200

    style: box-shadow-file

---
+++ src/styles/boxShadows.ts
@@ -0,0 +1,3 @@
+export const boxShadows = {
+  regular: '8px 8px 24px 0px rgba(66, 68, 90, 1)'
+};
--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,5 +1,7 @@
 import { colors } from './colors';
+import { boxShadows } from './boxShadows';
 
 export const theme = {
-  colors
+  colors,
+  boxShadows
 };
